ddf000d64de9f7d8d5133069f1550451c53b0807
duhuanpeng <u74147@gmail.com>
2020-04-04 17:51:25 +0800

    18.04: some some some

---
+++ initubuntu.sh
@@ -0,0 +1,22 @@
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+
+sudo apt autoremove nano -y
+adduser dialout $USER
+sudo apt install ckermit
+sudo apt install samba -y
+# edit /etc/samba/smb.conf
+sudo apt install tftpd-hpa -y
+# rename tftpboot 
+sudo apt install make -y
+sudo apt install gcc -y 
+sudo apt install gcc-8-mips64el-linux-gnuabi64 -y
+sudo apt install gcc-mips64el-linux-gnuabi64 
+sudo apt install libncurses5-dev -y
+sudo apt install tftpd-hpa -y
+sudo apt install openssh-server 
+sudo apt install vim -y
+sudo apt install tmux -y
+sudo apt install net-tools
+sudo apt install  lib32ncurses5 lib32z1 -y
+sudo apt install texlive-full -y
+
