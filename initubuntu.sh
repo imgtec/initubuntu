dafe357792fd4f023cadcf8b93118154304b345a
duhuanpeng <u74147@gmail.com>
2020-04-21 16:02:55 +0800

    kernel building tools
    Signed-off-by: duhuanpeng <u74147@gmail.com>

--- initubuntu.sh
+++ initubuntu.sh
@@ -1,7 +1,8 @@
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+sudo adduser du dialout
 
 sudo apt autoremove nano -y
-adduser dialout $USER
+# adduser dialout $USER
 sudo apt install ckermit
 sudo apt install samba -y
 # edit /etc/samba/smb.conf
@@ -11,6 +12,7 @@ sudo apt install make -y
 sudo apt install gcc -y 
 sudo apt install gcc-8-mips64el-linux-gnuabi64 -y
 sudo apt install gcc-mips64el-linux-gnuabi64 
+sudo apt install gcc-mipsel-linux-gnu
 sudo apt install libncurses5-dev -y
 sudo apt install tftpd-hpa -y
 sudo apt install openssh-server 
@@ -19,4 +21,10 @@ sudo apt install tmux -y
 sudo apt install net-tools
 sudo apt install  lib32ncurses5 lib32z1 -y
 sudo apt install texlive-full -y
+sudo apt install net-tools
+sudo apt install vim
+
+sudo apt install flex
+sudo apt install bison
+sudo apt install u-boot-tools
 
