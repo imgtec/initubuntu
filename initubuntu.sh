a58f41722481f527883623aa262f9b37c07ba423
duhuanpeng <u74147@gmail.com>
2020-04-27 16:56:45 +0800

    add "-y" arg
    Signed-off-by: duhuanpeng <u74147@gmail.com>

--- initubuntu.sh
+++ initubuntu.sh
@@ -11,7 +11,7 @@ sudo apt install tftpd-hpa -y
 sudo apt install make -y
 sudo apt install gcc -y 
 sudo apt install gcc-8-mips64el-linux-gnuabi64 -y
-sudo apt install gcc-mips64el-linux-gnuabi64 
+sudo apt install gcc-mips64el-linux-gnuabi64 -y
 sudo apt install gcc-mipsel-linux-gnu
 sudo apt install libncurses5-dev -y
 sudo apt install tftpd-hpa -y
@@ -19,21 +19,21 @@ sudo apt install openssh-server
 sudo apt install vim -y
 sudo apt install tmux -y
 sudo apt install net-tools
-sudo apt install  lib32ncurses5 lib32z1 -y
+sudo apt install lib32ncurses5 lib32z1 -y
 sudo apt install texlive-full -y
 sudo apt install net-tools
 sudo apt install vim
 
-sudo apt install flex
-sudo apt install bison
-sudo apt install u-boot-tools
-sudo apt  install exuberant-ctags
+sudo apt install -y flex
+sudo apt install -y bison
+sudo apt install -y u-boot-tools
+sudo apt install -y exuberant-ctags
 
 sudo apt install scons
-sudo apt install mutt
-sudo apt install lib32z1
+sudo apt install -y mutt
+sudo apt install -y lib32z1
 
-sudo apt install tree
+sudo apt install -y tree
 
 
 # internet
